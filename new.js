c7680e8789c53b8c875edb54f72b674bb0104266
samidha1703 <samiddhapatill@gmail.com>
2026-05-02 18:31:34 +0530

    Removed unnecessary files

--- new.js
+++
@@ -1,8 +0,0 @@
-var http=require('http');
-var fs=require('fs');
-var url=require('url');
-var addr="http://localhost:5000/default.html?name=scott"
-var result=url.parse(addr,true);
-console.log(result.host);
-console.log(result.search);
-console.log(result.pathname);
